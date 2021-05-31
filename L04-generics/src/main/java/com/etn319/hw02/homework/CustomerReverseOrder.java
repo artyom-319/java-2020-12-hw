91aca28bab8bf1f1689a9adb3e7488e1a62b08ef
artyom-319 <etn319@gmail.com>
2021-05-31 21:36:21 +0300

    CustomerReverseOrder implemented - 4th test now passes

--- L04-generics/src/main/java/com/etn319/hw02/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/com/etn319/hw02/homework/CustomerReverseOrder.java
@@ -1,16 +1,19 @@
 package com.etn319.hw02.homework;
 
 
+import java.util.Stack;
+
 public class CustomerReverseOrder {
+    private Stack<Customer> customerStack = new Stack<>();
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
     public void add(Customer customer) {
-
+        customerStack.push(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return customerStack.pop();
     }
 }
